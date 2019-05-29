52a2a0cc08f9d19a7df15993261f8704f9500726
Andy R <andyr914@gmail.com>
2019-05-29 09:21:44 -0700

    2nd commit 'looked up examples/videos and fixed line towards working code'

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var wins = 0;
+var losses = 0;
+// result is the total score added together
+var result = 0;
+var target;
+var random;
+var started = false;
+var card1;
+var card2;
+var card3;
+var card4;
+
+$(document).ready(function(){
+    startGame();
+})
+
+// Produces random number used to determine card amount
+function randNum() {
+    random = Math.round[Math.random() * 10];
+    console.log(random);
+}
+        
+randNum();
+
+
+// target {
+
+// }
+
+$("#card1").on("click", function(){
+
+
+})
